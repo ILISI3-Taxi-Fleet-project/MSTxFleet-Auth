78e814f074190dc3c10bfe6cd7ca8f2c2813c914
musdev <w0l1d.dev@gmail.com>
2023-10-24 21:20:18 +0100

    Initial commit

---
+++ src/main/java/com/ilisi/mstxfleetauth/MsTxFleetAuthApplication.java
@@ -0,0 +1,13 @@
+package com.ilisi.mstxfleetauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsTxFleetAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsTxFleetAuthApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ilisi/mstxfleetauth/MsTxFleetAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ilisi.mstxfleetauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsTxFleetAuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
