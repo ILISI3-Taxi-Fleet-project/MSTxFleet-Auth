4557919da07b80e5e3cb1cfd1fd5f6f385f5a7c4
abdo <abdo-pro@hotmail.com>
2023-11-22 17:46:42 +0100

    #9 Added userId to the authentication response

--- src/main/java/com/ilisi/mstxfleetauth/web/AuthenticationController.java
+++ src/main/java/com/ilisi/mstxfleetauth/web/AuthenticationController.java
@@ -1,6 +1,6 @@
 package com.ilisi.mstxfleetauth.web;
 
-import com.ilisi.mstxfleetauth.dto.UserDTO;
+import com.ilisi.mstxfleetauth.entity.AppUser;
 import com.ilisi.mstxfleetauth.services.AuthenticationService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -21,16 +21,16 @@ public class AuthenticationController {
     private final AuthenticationService authenticationService;
 
     @PostMapping("/login")
-    public Map<String, Object> login(@RequestBody UserDTO userDTO) {
-        log.info("Received login request for username: {}", userDTO.getUsername());
+    public Map<String, Object> login(@RequestBody AppUser appUser) {
+        log.info("Received login request for username: {}", appUser.getUsername());
 
         try {
             // Authentication logic
-            Map<String, Object> tokens = authenticationService.authenticate(userDTO);
-            log.info("Authentication successful for user: {}", userDTO.getUsername());
+            Map<String, Object> tokens = authenticationService.authenticate(appUser);
+            log.info("Authentication successful for user: {}", appUser.getUsername());
             return tokens;
         } catch (AuthenticationException e) {
-            log.error("Authentication failed for user: {}", userDTO.getUsername(), e);
+            log.error("Authentication failed for user: {}", appUser.getUsername(), e);
             throw e; // Rethrow the exception for handling in a global exception handler, if configured.
         }
     }
